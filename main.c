ed7ba09aae12c05d015b7240140d7ba670d894ab
User <wocc981@yandex.ru>
2018-03-14 23:03:48 +0000

    add main.c

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+      printf("8 r n b q k b n r\n");
+      printf("7 p p p p p p p p\n");
+      printf("6\n");
+      printf("5\n");
+      printf("4\n");
+      printf("3\n");
+      printf("2 P P P P P P P P\n");
+      printf("1 R N B Q K B N R\n");
+      printf("  a b c d e f g h\n");
+}
